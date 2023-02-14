4c5bc177a4ee50683f212ccf42bd967cceb3ae85
Samir Hammoudi <ksimir@gmail.com>
2023-02-14 14:05:09 +0900

    Initial Commit

---
+++ techGoods/techGoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  techGoodsApp.swift
+//  techGoods
+//
+//  Created by Samir Hammoudi on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct techGoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
